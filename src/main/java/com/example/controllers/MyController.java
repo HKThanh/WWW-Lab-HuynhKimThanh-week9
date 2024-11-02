ec80630597c95ce894e9cb423305280e755d9ed2
H82M19\Student <Student>
2024-11-02 19:45:24 +0700

    Adding multiply table with a given value in url

--- src/main/java/com/example/controllers/MyController.java
+++ src/main/java/com/example/controllers/MyController.java
@@ -3,11 +3,15 @@ package com.example.controllers;
 import com.example.services.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/sample")
@@ -29,6 +33,16 @@ public class MyController {
         ModelAndView mav = new ModelAndView("add");
 
         mav.addObject("add", service.add(a, b));
+        return mav;
+    }
+
+    @RequestMapping(value = {"multiply/{a}"}, method = RequestMethod.GET)
+    public ModelAndView multiply(@PathVariable(value = "a") int a) {
+        ModelAndView mav = new ModelAndView("multiply");
+        int i = 1;
+
+        List<Integer> multiplies = service.multiplyTo10(a);
+
         return mav;
     }
 }
--- src/main/java/com/example/services/MyService.java
+++ src/main/java/com/example/services/MyService.java
@@ -2,9 +2,22 @@ package com.example.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MyService {
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public List<Integer> multiplyTo10(int a) {
+        List<Integer> multiplies = new ArrayList<>();
+
+        for (int i = 1; i <= 10 ; i++) {
+            multiplies.add(a * i);
+        }
+
+        return multiplies;
     }
 }
